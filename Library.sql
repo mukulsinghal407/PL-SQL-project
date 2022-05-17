ce15d122adea0b5e2ce00443f210d928a1c2626f
Mukul  Singhal <79706982+mukulsinghal407@users.noreply.github.com>
2022-05-17 12:19:16 +0530

    test2

--- Library.sql
+++ Library.sql
@@ -1,260 +1,47 @@
--- Login for customer
+-- This is used to update the information of the customers if required
 DECLARE 
-    PROCEDURE loginCustomer_library(user IN VARCHAR2, pass IN VARCHAR2) 
+  auxCustomer customer.customerid%TYPE; 
+  pNumber NUMBER; 
+  address VARCHAR(20); 
+  newPass VARCHAR(20); 
+  PROCEDURE updateInfoCusto_library(auxCustomer IN customer.customerid%TYPE, pNumber NUMBER, address VARCHAR2, newPass VARCHAR2) 
     IS 
-      passAux customer.password%TYPE; 
-      incorrect_password EXCEPTION; 
     BEGIN 
-      SELECT password INTO passAux 
-      FROM customer 
-      WHERE username LIKE user; 
-       
-      IF passAux LIKE pass THEN 
-        DBMS_OUTPUT.PUT_LINE('User ' || user || ' loging succesfull'); 
-      ELSE 
-        RAISE incorrect_password; 
-      END IF; 
-       
-      EXCEPTION 
-      WHEN no_data_found OR incorrect_password THEN  
-           DBMS_OUTPUT.PUT_LINE('Incorrect username or password'); 
-                                        
+      UPDATE customer 
+      SET phone = pNumber, customeraddress = address, password = newPass 
+      WHERE customerid = auxCustomer;
+      DBMS_OUTPUT.PUT_LINE('Successfully Updated customer table');
     END; 
 BEGIN 
-  loginCustomer_library('al1','alfred123'); 
-END;
-
-
--- Login for Employee
-DECLARE 
-  PROCEDURE loginEmployee_library(user IN VARCHAR2, pass IN VARCHAR2) 
-  IS 
-    passAux employee.password%TYPE; 
-   incorrect_password EXCEPTION; 
-  BEGIN 
-    SELECT password INTO passAux 
-    FROM employee 
-    WHERE username LIKE user; 
-    IF passAux LIKE pass THEN 
-     DBMS_OUTPUT.PUT_LINE('User ' || user || ' loging succesfull'); 
-    ELSE 
-    RAISE incorrect_password; 
-    END IF; 
-    EXCEPTION 
-      WHEN no_data_found OR incorrect_password THEN  
-           DBMS_OUTPUT.PUT_LINE('Incorrect username or password'); 
-    END; 
-BEGIN 
-  loginEmployee_library('ro11','ross123'); 
-END;
-
-
-DECLARE 
-  auxItemID VARCHAR2(10); 
-    PROCEDURE viewItem_library(auxItemID IN VARCHAR2) 
-    IS 
-      auxISBN VARCHAR2(4); 
-      auxState VARCHAR2(10); 
-      auxDebyCost NUMBER(10,2); 
-      auxLostCost NUMBER(10,2); 
-      auxAddress VARCHAR2(50); 
-      auxAbala VARCHAR2(1); 
-      auxBook NUMBER; 
-    BEGIN 
-      SELECT COUNT(*) INTO auxBook 
-      FROM book 
-      WHERE bookid LIKE auxItemID; 
-       
-      IF auxBook > 0 THEN 
-        SELECT isbn, state, avalability, debycost, lostcost, address 
-        INTO auxISBN, auxState, auxAbala, auxDebyCost, auxLostCost, auxAddress 
-        FROM book 
-        WHERE bookid LIKE auxItemID; 
-       
-        DBMS_OUTPUT.PUT_LINE('BOOK ' || auxItemID || ' INFO'); 
-        DBMS_OUTPUT.PUT_LINE('------------------------------------------'); 
-        DBMS_OUTPUT.PUT_LINE('ISBN: ' || auxISBN); 
-        DBMS_OUTPUT.PUT_LINE('STATE: ' || auxState); 
-        DBMS_OUTPUT.PUT_LINE('AVALABILITY: ' || auxAbala); 
-        DBMS_OUTPUT.PUT_LINE('DEBY COST: ' || auxDebyCost); 
-        DBMS_OUTPUT.PUT_LINE('LOST COST: ' || auxLostCost); 
-        DBMS_OUTPUT.PUT_LINE('ADDRESS: ' || auxAddress); 
-        DBMS_OUTPUT.PUT_LINE('------------------------------------------'); 
-      END IF; 
-    END; 
-BEGIN 
-  auxItemID :='B1B234'; 
-  viewItem_library(auxItemID); 
-END;
-
-
-
---CUSTOMER-- 
-DECLARE 
-  custoID customer.customerid%TYPE; 
-  PROCEDURE customerAccount_library(custoID IN customer.customerid%TYPE) 
-    IS 
-      auxCard NUMBER; 
-      auxFines NUMBER; 
-      auxItem VARCHAR(6); 
-      rented number := 0; 
-    BEGIN 
-      SELECT cardnumber INTO auxCard 
-      FROM customer 
-      WHERE customerid LIKE custoID; 
-       
-      SELECT COUNT(*) INTO rented 
-      FROM rent 
-      WHERE rent.cardid LIKE auxcard; 
-       
-      DBMS_OUTPUT.PUT_LINE('The user card is ' || auxCard);   
-      IF (rented > 0) THEN 
-        SELECT rent.itemid INTO auxItem 
-        FROM rent,card 
-        WHERE card.cardid = rent.cardid 
-        AND card.cardid LIKE auxCard;     
-         
-        DBMS_OUTPUT.PUT_LINE('The user has ' || auxItem || ' rented'); 
-      ELSE     
-        DBMS_OUTPUT.PUT_LINE('This user has no rents');  
-      END IF; 
-       
-      SELECT fines INTO auxFines 
-      FROM card 
-      WHERE cardid LIKE auxcard; 
-       
-      DBMS_OUTPUT.PUT_LINE('The user fines are ' || auxFines); 
-         
-      EXCEPTION WHEN no_data_found THEN  
-      DBMS_OUTPUT.PUT_LINE('NOT DATA FOUND'); 
-    END; 
-BEGIN 
-  custoID := 4;  
-  customerAccount_library(custoID); 
-END;
-
-
-
---EMPLOYEE-- 
-DECLARE 
-  emploID employee.employeeid%TYPE; 
- PROCEDURE employeeAccount_library(emploID IN employee.employeeid%TYPE) 
-    IS 
-      auxCard NUMBER; 
-      auxFines NUMBER; 
-      auxItem VARCHAR(6); 
-      rented number := 0; 
-    BEGIN 
-      SELECT cardnumber INTO auxCard 
-      FROM employee 
-      WHERE employeeid LIKE emploID; 
-       
-      SELECT COUNT(*) INTO rented 
-      FROM rent 
-      WHERE rent.cardid LIKE auxcard; 
-       
-      DBMS_OUTPUT.PUT_LINE('The user card is ' || auxCard);   
-      IF (rented > 0) THEN 
-        SELECT rent.itemid INTO auxItem 
-        FROM rent,card 
-        WHERE card.cardid = rent.cardid 
-        AND card.cardid LIKE auxCard;     
-         
-        DBMS_OUTPUT.PUT_LINE('The user has ' || auxItem || ' rented'); 
-      ELSE     
-        DBMS_OUTPUT.PUT_LINE('This user has no rents');  
-      END IF; 
-       
-      SELECT fines INTO auxFines 
-      FROM card 
-      WHERE cardid LIKE auxcard; 
-       
-      DBMS_OUTPUT.PUT_LINE('The user fines are ' || auxFines); 
-         
-      EXCEPTION WHEN no_data_found THEN  
-      DBMS_OUTPUT.PUT_LINE('NOT DATA FOUND'); 
-    END; 
-BEGIN 
-  emploID := 211; 
-  employeeAccount_library(emploID); 
-END;
-
-
---4-- 
-DECLARE 
-  auxCard NUMBER; 
-  auxItemID VARCHAR2(10); 
-  auxDate DATE; 
-  PROCEDURE rentItem_library(auxCard IN NUMBER, auxItemID IN VARCHAR2, auxDate IN DATE) 
-  IS 
-   statusAux VARCHAR2(1); 
-   itemStatus VARCHAR2(1); 
-  BEGIN 
-    SELECT status INTO statusAux 
-    FROM card 
-    WHERE cardid LIKE auxCard; 
-    IF statusAux LIKE 'A' THEN 
-     SELECT avalability INTO itemStatus 
-     FROM book 
-     WHERE bookid LIKE auxItemID; 
-     IF itemStatus LIKE 'A' THEN 
-       UPDATE book 
-       SET avalability = 'O' 
-       WHERE bookid LIKE auxItemID; 
-       INSERT INTO rent VALUES (auxCard,auxItemID,sysdate,auxDate); 
-       DBMS_OUTPUT.PUT_LINE('Item ' || auxItemID || ' rented'); 
-     ELSE 
-        DBMS_OUTPUT.PUT_LINE('The item is already rented'); 
-     END IF; 
-    ELSE 
-     DBMS_OUTPUT.PUT_LINE('The user is blocked'); 
-    END IF;     
-  END; 
-BEGIN 
-  auxCard := 101;
-  auxItemID := 'B2A123';
-  auxDate := '20-May-2018';
-  rentItem_library(auxCard,auxItemID,auxDate); 
-END;
-
---5-- 
-DECLARE 
-  auxCard card.cardid%TYPE; 
-  money NUMBER; 
-  PROCEDURE payFines_library(auxCard IN card.cardid%TYPE, money IN NUMBER) 
-    IS 
-      finesAmount NUMBER; 
-      total NUMBER; 
-    BEGIN 
-      SELECT fines INTO finesAmount 
-      FROM card 
-      WHERE cardid LIKE auxCard; 
-       
-      IF finesAmount < money THEN 
-        total := money - finesAmount; 
-        DBMS_OUTPUT.PUT_LINE('YOU HAVE PAYED ALL YOUR FINES AND YOU HAVE ' || total || ' MONEY BACK'); 
-        UPDATE card 
-        SET status = 'A', fines = 0 
-        WHERE cardid = auxCard; 
-      ELSIF finesAmount = money THEN 
-        total := money - finesAmount; 
-        DBMS_OUTPUT.PUT_LINE('YOU PAY ALL YOUR FINES'); 
-        UPDATE card 
-        SET status = 'A', fines = 0 
-        WHERE cardid = auxCard; 
-      ELSE 
-        total := finesAmount - money; 
-        DBMS_OUTPUT.PUT_LINE('YOU WILL NEED TO PAY ' || total || ' MORE DOLLARS TO UNLOCK YOUR CARD'); 
-        UPDATE card 
-        SET fines = total 
-        WHERE cardid = auxCard; 
-      END IF; 
-    END; 
-BEGIN 
-  auxCard := 101; 
-  money := 100;
-  payFines_library(auxCard,money); 
+  auxCustomer := 4; 
+  pNumber := 623623623; 
+  address := 'WASHINGTON DC.'; 
+  newPass := 'tom123'; 
+  updateInfoCusto_library(auxCustomer,pNumber,address,newPass); 
 END; 
 
-
-
+-- This is used to update the information of the employee if required
+DECLARE 
+  auxEmployee employee.employeeid%TYPE; 
+  pNumber NUMBER; 
+  address VARCHAR(45); 
+  newPass VARCHAR(45); 
+  newPayCheck NUMBER; 
+  newBranch VARCHAR(45); 
+  PROCEDURE updateInfoEmp_library(auxEmployee IN employee.employeeid%TYPE, pNumber NUMBER, address VARCHAR2, newPass VARCHAR2, newPayCheck NUMBER, newBranch VARCHAR2) 
+    IS 
+    BEGIN 
+      UPDATE employee 
+      SET phone = pNumber, EMPLOYEEADDRESS = address, password = newPass, paycheck = auxEmployee, branchname = newBranch 
+      WHERE employeeid = auxEmployee;
+      DBMS_OUTPUT.PUT_LINE('Successfully Updated employee table');
+END; 
+BEGIN 
+  auxEmployee := 211; 
+  pNumber := 623623623; 
+  address := 'HIS HOUSE'; 
+  newPass := 'ross123'; 
+  newPayCheck := 1300; 
+  newBranch := 'COMPUTING'; 
+  updateInfoEmp_library(auxEmployee,pNumber,address,newPass,newPayCheck,newBranch); 
+END; 
