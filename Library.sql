319048b4f2ecda5acf7ade4f4296e867f07e9735
Mukul  Singhal <79706982+mukulsinghal407@users.noreply.github.com>
2022-05-17 12:50:25 +0530

    c

--- Library.sql
+++ Library.sql
@@ -1,36 +1,33 @@
--- This function is used to handle the return of items and modify the status of the given item in all the tables 
---affected by it.
+-- This trigger has been made to maintain refential integrity of the tables when there is any deletion in the rent table 
+CREATE OR REPLACE TRIGGER modifyFines_library 
+AFTER DELETE 
+ON rent 
+FOR EACH ROW 
 DECLARE 
-  auxItemID VARCHAR(10); 
-  PROCEDURE handleReturns_library(auxItemID IN VARCHAR) 
-    IS 
-      auxRented NUMBER; 
-      auxBook NUMBER; 
-    BEGIN 
-      SELECT COUNT(*) INTO auxRented 
-      FROM rent 
-      WHERE itemid LIKE auxItemID; 
-      
-      SELECT COUNT(*) INTO auxBook 
+  auxCardID NUMBER; 
+  auxItemID VARCHAR(6); 
+  auxBook NUMBER; 
+  auxDeby NUMBER; 
+BEGIN   
+  SELECT cardid, itemid INTO auxCardID, auxItemID 
+  FROM rent 
+  WHERE cardid LIKE :old.cardid; 
+   
+  SELECT COUNT(*) INTO auxBook 
+  FROM book 
+  WHERE bookid LIKE auxItemID; 
+   
+  IF sysdate > :old.returndate THEN 
+    IF auxBook > 0 THEN 
+      SELECT debyCost INTO auxDeby 
       FROM book 
       WHERE bookid LIKE auxItemID; 
-      
-      IF auxRented > 0 THEN 
-        DELETE FROM rent 
-        WHERE itemid = auxItemID; 
-        IF auxBook > 0 THEN 
-          UPDATE book 
-          SET avalability = 'A' 
-          WHERE bookid LIKE auxItemID; 
-          DBMS_OUTPUT.PUT_LINE('The book ' || auxItemID || ' is now avaible.'); 
-        END IF; 
-      ELSE 
-        DBMS_OUTPUT.PUT_LINE('This item is not rented at the moment'); 
-      END IF; 
-      EXCEPTION WHEN no_data_found THEN  
-      DBMS_OUTPUT.PUT_LINE('Item ID incorrect');     
-    END; 
-BEGIN 
-  auxItemID := 'B1A123'; 
-  handleReturns_library(auxItemID); 
-END; 
+    END IF; 
+     
+    UPDATE card 
+    SET status = 'B', fines = (fines + auxDeby) 
+    WHERE cardid LIKE auxCardID; 
+  ELSE 
+    DBMS_OUTPUT.PUT_LINE('The item has been return before deadline'); 
+  END IF; 
+END;
