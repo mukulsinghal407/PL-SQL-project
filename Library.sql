4357029b798618e483c4db21ef30c5770af5d881
Mukul  Singhal <79706982+mukulsinghal407@users.noreply.github.com>
2022-05-17 13:10:22 +0530

    books final

--- Library.sql
+++ Library.sql
@@ -1,25 +1,17 @@
---this procedure is used to add new books in the library database-- 
-DECLARE 
-  auxISBN VARCHAR2(4); 
-  auxItemID VARCHAR2(6); 
-  auxState VARCHAR2(10); 
-  auxDebyCost NUMBER(10,2); 
-  auxLostCost NUMBER(10,2); 
-  auxAddress VARCHAR2(50); 
-    PROCEDURE addBook_library(auxISBN IN VARCHAR2, auxBookID IN VARCHAR2, auxState IN VARCHAR2, auxDebyCost IN NUMBER, 
-    auxLostCost IN NUMBER, auxAddress IN VARCHAR2) 
-    IS 
-    BEGIN 
-      INSERT INTO book 
-      VALUES(auxISBN,auxBookID,auxState,'A',auxDebyCost,auxLostCost,auxAddress); 
-      DBMS_OUTPUT.PUT_LINE('Book inserted correctly'); 
-    END; 
+--This Cursor is used to print the details of all the books in the library
+DECLARE
+ CURSOR cBooks IS
+ select * from book;
+ xBooks cBooks%ROWTYPE; 
 BEGIN 
-    auxISBN := 'D123'; 
-    auxItemID := 'B2B234'; 
-    auxState := 'NEW'; 
-    auxDebyCost := 5; 
-    auxLostCost := 15; 
-    auxAddress := 'CHEMISTRY ROAD'; 
-    addBook_library(auxISBN, auxItemID, auxState, auxDebyCost, auxLostCost, auxAddress); 
-END; 
+    OPEN cBooks; 
+    DBMS_OUTPUT.PUT_LINE('ISBN     ID     STATE     AVALABILITY     DEBY_COST     LOST_COST    LOCATION'); 
+    DBMS_OUTPUT.PUT_LINE('-----------------------------------------------------------------------------'); 
+    LOOP 
+      FETCH cBooks 
+      INTO xBooks; 
+      EXIT WHEN cBooks%NOTFOUND; 
+      DBMS_OUTPUT.PUT_LINE(xBooks.isbn || '     ' || xBooks.bookid || '     ' || xBooks.state || '     ' || xBooks.avalability || '     ' || xBooks.debycost || '     ' || xBooks.lostcost || '     ' || xBooks.address); 
+    END LOOP; 
+    CLOSE cBooks;
+END;
